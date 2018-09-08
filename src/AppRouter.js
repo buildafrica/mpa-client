867947d3b713fa0d610de138985e903e0be38abd
Andrew <me@andrewmiracle.com>
2018-09-08 21:37:45 +0100

    create Reset password component and import Authentication layer (login,signup,passreset) into AppRouter

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -4,8 +4,10 @@ import './App.css';
 import {BrowserRouter as Router, Route, Switch} from  'react-router-dom';
 import About from './components/About';
 import Archive from './components/Archive';
-import Login from './components/Login';
 import Home from './components/Home';
+import Login from './components/Login';
+import Signup from './components/Signup';
+import ResetPassword from './components/ResetPassword';
 import FourOhFour from './components/FourOhFour';
 
 
@@ -22,6 +24,8 @@ class App extends Component {
           <Route path="/about" component={About} />
           <Route path="/archive" component={Archive} />
           <Route path="/login" component={Login} />
+          <Route path="/signup" component={Signup} />
+          <Route path="/resetpass" component={ResetPassword} />
           <Route component={FourOhFour} />
         </Switch>
         </section>
